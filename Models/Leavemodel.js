0a1f38f171a434da16af05b5134ce38560db5016
selvapriyasuhana <142295945+selvapriyasuhana@users.noreply.github.com>
2023-10-13 16:08:30 +0530

    Update Leavemodel.js

--- Models/Leavemodel.js
+++ Models/Leavemodel.js
@@ -41,52 +41,6 @@ const leaveSchema = new mongoose.Schema({
     default: "pending",
   },
 });
-/*leaveSchema.path("StartDate").validate(async function (StartDate) {
-  const existingRequest = await mongoose.models.Leave.findOne({
-    StartDate,
-    username: this.username,
-  });
-
-  if (existingRequest) {
-    return true;
-  }
-
-  return false;
-});
-// Validate uniqueness of StartDate per user
-/*leaveSchema.path("username").validate(async function (username) {
-  const existingRequest = await mongoose.model("Leave").findOne({
-    StartDate: this.StartDate,
-    username,
-  });
-
-  return !existingRequest;
-}, "Leave request for this date already exists for this user");
-//leaveSchema.index({ username: 1 }, { unique: false });
-//leaveSchema.index({ username: 1, StartDate: 1, EndDate: 1 }, { unique: true });
-*/
-// Validate uniqueness of StartDate and EndDate per user
-/*leaveSchema.path("username").validate(async function (username) {
-  const existingRequest = await mongoose.model("Leave").findOne({
-    username,
-    $or: [
-      {
-        $and: [
-          { StartDate: { $lte: this.StartDate } },
-          { EndDate: { $gte: this.StartDate } },
-        ],
-      },
-      {
-        $and: [
-          { StartDate: { $lte: this.EndDate } },
-          { EndDate: { $gte: this.EndDate } },
-        ],
-      },
-    ],
-  });
-
-  return !existingRequest;
-}, "Leave request for this date range already exists for this user");*/
 
 const Leave = mongoose.model("Leave", leaveSchema);
 
